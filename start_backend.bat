5a08a5e782607d04b459eabe62c69331595bacfb
MohdAwf <awfbobber@gmail.com>
2025-07-20 13:03:35 +0400

    Removed some unrelevant files and folders

--- start_backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd backend
-call .\venv\Scripts\activate.bat
-uvicorn server:app --reload 
--- start_backend.ps1
+++
@@ -1,4 +0,0 @@
-cd backend
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-.\venv\Scripts\Activate.ps1
-uvicorn server:app --reload 
